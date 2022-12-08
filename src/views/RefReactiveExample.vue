6553364293a13dcb401810cf629ddf30110c9ebb
ketyykes <xi4869@gmail.com>
2022-12-08 21:51:59 +0800

    wathcRef和Reactive的差異

--- src/views/RefReactiveExample.vue
+++ src/views/RefReactiveExample.vue
@@ -1,25 +1,25 @@
 <script setup>
-import { reactive, ref } from "vue";
-const name = ref("Tom");
-const person = reactive({
+import { reactive, ref, watch } from "vue";
+const person1 = ref({
+	name: "Tom",
+	age: 24,
+	address: "台北",
+});
+const person2 = reactive({
 	name: "Danny",
 	age: 18,
 	address: "台南",
+});
+setTimeout(() => {
+	person1.value.name = "Hello";
+	person2.name = "Hello";
+}, 2000);
+watch(person1, () => {
+	console.log("person1被改變了");
+});
+watch(person2, () => {
+	console.log("person2被改變了");
 });
 </script>
 
-<template>
-	<div>{{ person.name }}</div>
-	<div>{{ person.age }}</div>
-	<div>{{ person.address }}</div>
-</template>
-<script setup>
-import { reactive, ref } from "vue";
-const name = ref("Tom");
-const person = reactive({
-	name: "Danny",
-	age: 18,
-	address: "台南",
-});
-person.name = "Hello";
-</script>
+<template></template>
