46eaa47220f273ebe5c24dd313d13864b81b5f4f
ketyykes <xi4869@gmail.com>
2022-12-08 18:14:45 +0800

    使用setup語法糖 傳入component需要定義型別

--- src/views/CompositionAPI.vue
+++ src/views/CompositionAPI.vue
@@ -1,28 +1,20 @@
-<script>
+<script setup>
 import { onMounted, ref } from "vue";
 import HelloWorld from "../components/HelloWorld.vue";
-export default {
-	components: {
-		HelloWorld,
+const props = defineProps({
+	data: {
+		type: Object,
+		default: () => ({}),
 	},
-	props: {
-		data: {
-			type: Object,
-			default: () => ({}),
-		},
-	},
-	setup() {
-		const count = ref(0);
+});
+const count = ref(0);
 
-		const increment = () => {
-			count.value += 1;
-		};
-		onMounted(() => {
-			console.log(count.value);
-		});
-		return { count, increment };
-	},
+const increment = () => {
+	count.value += 1;
 };
+onMounted(() => {
+	console.log(count.value);
+});
 </script>
 
 <template>
